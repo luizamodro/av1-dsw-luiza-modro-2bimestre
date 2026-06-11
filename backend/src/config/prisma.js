016d23079194586f7bc58c6203058a567b1ca2fb
luizamodro <luizamodro>
2026-06-11 10:57:25 -0300

    certo

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,17 +1,6 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../generated/prisma/index.js";
 
 const prisma = new PrismaClient();
 
-async function verifyConnection() {
-  try {
-    await prisma.$connect();
-    console.log("Conexão bem-sucedida com o banco de dados!");
-    return true;
-  } catch (error) {
-    console.error("Erro ao conectar ao banco de dados:", error);
-    return false;
-  }
-}
-
-export { prisma, verifyConnection };
+export { prisma };
